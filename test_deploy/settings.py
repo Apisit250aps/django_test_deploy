bedba03c3c34b4efba77741bb4643b4c4b732959
YUapisit <88958116+Apisit250aps@users.noreply.github.com>
2024-07-31 10:34:47 +0700

    allow host

--- test_deploy/settings.py
+++ test_deploy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-g60(j6nmg)vka&p0#p$z64xawyejj*gqcu4(r@4ym6fpu0$62u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*.vercel.app"]
 
 
 # Application definition
